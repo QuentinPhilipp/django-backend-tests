3c3f6742404886ac5cbaafb335e2d5571ce08362
Quentin Philipp <philippquentin6@gmail.com>
2022-12-29 17:22:27 +0100

    Add unittests

--- activity_tracker/models.py
+++ activity_tracker/models.py
@@ -6,10 +6,10 @@ from django.utils import timezone
 # Create your models here.
 class Athlete(models.Model):
     username=models.CharField(max_length=200)
-    creation_date=models.DateTimeField('Account creation')
+    creation_date=models.DateTimeField('Account creation', default=timezone.now)
 
     def __str__(self):
-        return f"{self.id} - {self.username}"
+        return f"<User {self.id} - {self.username}>"
 
 class Activity(models.Model):
     athlete = models.ForeignKey(Athlete, on_delete=models.CASCADE)
@@ -18,4 +18,5 @@ class Activity(models.Model):
     start_date=models.DateTimeField('Activity date', default=timezone.now)
 
     def was_published_recently(self):
-        return self.start_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now-datetime.timedelta(days=1) <= self.start_date <= now
--- activity_tracker/tests.py
+++ activity_tracker/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Activity, Athlete
+
+class ActivityModelTests(TestCase):
+
+    def setUp(self):
+        Athlete.objects.create(username='TestUser')
+
+    def test_was_published_recently_with_future_date(self):
+        """
+        was_published_recently() return False for questions whose start_date are
+        in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        athlete = Athlete.objects.get(username='TestUser')
+        futureActivity = Activity(athlete=athlete, start_date=time)
+
+        self.assertFalse(futureActivity.was_published_recently())
+
+    def test_was_published_recently_with_old_date(self):
+        """
+        was_published_recently() return False for questions whose start_date are
+        older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        athlete = Athlete.objects.get(username='TestUser')
+        pastActivity = Activity(athlete=athlete, start_date=time)
+
+        self.assertFalse(pastActivity.was_published_recently())
+
+    def test_was_published_recently_with_recent_date(self):
+        """
+        was_published_recently() return True for questions whose start_date are
+        within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        athlete = Athlete.objects.get(username='TestUser')
+        recentActivity = Activity(athlete=athlete, start_date=time)
+
+        self.assertTrue(recentActivity.was_published_recently())
